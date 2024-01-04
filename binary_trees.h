4e1767584c92c2913f3febacb057aeb85b8b8e49
NathanielWatife <awosanminathaniel0@gmail.com>
2024-01-04 05:59:46 +0100

    worked on the 0-binary-tree-node.c

---
+++ binary_trees.h
@@ -0,0 +1,55 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+#define MAX(a,b) ((a) > (b) ? : (b))
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/**
+ * Binary search tree
+*/
+typedef struct binary_tree_s bst_t;
+
+/**
+ * AVL Tree
+*/
+typedef struct binary_tree_s avl_t;
+
+/**
+ * Max Binary Heap
+*/
+typedef struct binary_tree_s heap_t;
+
+/**
+ * Prototype functions
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+
+#endif 
